9deeab985fcae5072675db251ac23166c53c4199
prasanthmurugasamy <prasanth.m@fr8.in>
2022-01-05 18:30:10 +0530

    completed

---
+++ TASK/JS_Exercise/Ex2.js
@@ -0,0 +1,5 @@
+let owner_code = 'We act like owners.We are frugal.We value our "Money","Time & Resources" and ensure they are deployed in most productive way'
+for(let i=0;i<=100;i++)
+{
+    console.log(owner_code)
+}
